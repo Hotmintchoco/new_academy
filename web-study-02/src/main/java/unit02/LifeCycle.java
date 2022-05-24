ce58a8f8779e9199a9b74b1aea4bf42247eedf6f
hotchoco <yumkl123@gmail.com>
2022-05-24 16:45:38 +0900

    학원

---
+++ web-study-02/src/main/java/unit02/LifeCycle.java
@@ -0,0 +1,26 @@
+package unit02;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LifeCycle")
+public class LifeCycle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	public void init() throws ServletException {
+		System.out.println("init Method()");
+	}
+	
+	public void destroy() {
+		System.out.println("destroy Method()");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet Method()");
+	}
+}
