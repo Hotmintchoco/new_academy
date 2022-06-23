19edc5409b6e32f3c8ef323513ca99f3d0d8f0d8
hotchoco <yumkl123@gmail.com>
2022-06-23 12:43:19 +0900

    학원 18(AOP/pointcut_aspect)

--- BoardWeb/src/main/java/com/springbook/biz/user/impl/UserDAO.java
+++ BoardWeb/src/main/java/com/springbook/biz/user/impl/UserDAO.java
@@ -4,10 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.springframework.stereotype.Repository;
+
 import com.springbook.biz.common.JDBCUtil;
 import com.springbook.biz.user.UserService;
 import com.springbook.biz.user.UserVO;
 
+@Repository("userDAO")
 public class UserDAO implements UserService {
 	private Connection conn = null;
 	private PreparedStatement stmt = null;
