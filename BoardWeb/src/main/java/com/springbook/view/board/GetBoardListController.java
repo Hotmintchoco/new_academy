b85eb8bf75a0288c5f05f9b7dc41964c53240418
hotchoco <yumkl123@gmail.com>
2022-07-01 17:54:57 +0900

    학원 20

---
+++ BoardWeb/src/main/java/com/springbook/view/board/GetBoardListController.java
@@ -0,0 +1,34 @@
+package com.springbook.view.board;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAO;
+
+public class GetBoardListController implements Controller{
+
+   @Override
+   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+      System.out.println("글 목록 조회 처리");
+      
+      BoardVO vo = new BoardVO();
+      BoardDAO boardDAO = new BoardDAO();
+      List<BoardVO> boardList = boardDAO.getBoardList(vo);
+//      3. 검색 결과를 세션에 저장하고 목록 화면으로 이동한다.
+//      HttpSession session = request.getSession();
+//      session.setAttribute("boardList", boardList);
+//      return "getBoardList";
+      
+      ModelAndView mav = new ModelAndView();
+      mav.addObject("boardList", boardList);
+      mav.setViewName("getBoardList.jsp");
+      return mav;
+   }
+   
+}
---
+++ BoardWeb/src/main/webapp/insertBoard.jsp
@@ -0,0 +1,46 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<style type="text/css">
+	h1, h3 { text-align: center; }
+	table{ margin : 0 auto; }
+	div { text-align: center; }
+</style>
+</head>
+<body>
+<h1>글 상세</h1>
+<div>
+	<a href="logout_proc.jsp">Log-out</a>
+</div>
+<hr>
+<form action="insertBoard.do" method="post">
+	<table border="1">
+		<tr>
+			<td bgcolor="orange" width="70">제목</td>
+			<td align="left"><input type="text" name="title"></td>
+		</tr>
+		<tr>
+			<td bgcolor="orange">작성자</td>
+			<td align="left"><input type="text" name="writer"></td>
+		</tr>
+		<tr>
+			<td bgcolor="orange">내용</td>
+			<td><textarea cols="40" rows="10" name="content"></textarea></td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center">
+				<input type="submit" value="글 추가">
+			</td>
+		</tr>
+	</table>
+</form>
+<hr>
+<div>
+	<a href="getBoardList.do">글 목록 가기</a>
+</div>
+</body>
+</html>
