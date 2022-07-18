0e8d05924b9deed650f553cd63c1c1ce5842ea43
hotchoco <yumkl123@gmail.com>
2022-07-18 17:21:00 +0900

    학원 28

---
+++ ex02_1/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,71 @@
+package org.zerock.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import oracle.jdbc.proxy.annotation.Post;
+
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+	
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list---------------------");
+		model.addAttribute("list", service.getList());
+	}
+	
+	@GetMapping("/register")
+	public void register() {
+		
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO vo, RedirectAttributes rttr) {
+		log.info("register : " + vo);
+		service.register(vo);
+		rttr.addFlashAttribute("result", vo.getBno());
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping({"/get", "/modify"})
+	public void get(Long bno, Model model) {
+		log.info("----------- get or modify --------------------");
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	@PostMapping("/remove")
+	public String remove(Long bno, RedirectAttributes rttr) {
+		System.out.println(bno);
+		log.info("remove---------" + bno);
+		if(service.remove(bno)==1) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO vo, RedirectAttributes rttr) {
+		log.info("modify : " + vo);
+		if( service.modify(vo) == 1) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+		
+}
